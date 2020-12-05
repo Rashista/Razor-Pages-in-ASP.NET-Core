ab88729a757b0e192fdf6f1010f967fec0fb0b87
Yordan Rashkov <jrashkov@gmail.com>
2020-12-05 21:00:16 +0200

    Add Validations
    DataAnnotations

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -10,16 +10,26 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
+        [Required]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 }
